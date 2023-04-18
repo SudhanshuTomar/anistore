4b298fdbb512765d06651975d5c63cdc51bdebeb
Sudhanshu <deepak@Deepaks-MacBook-Air.local>
2023-04-18 14:52:33 +0530

    styled homepage and divided into components

---
+++ src/components/menu-item/menu-item.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Menuitem({title,id,imgurl,size}) {
+  return (
+    <div className={`${size} menu-item`}>
+
+      <div className = "background-image" style = {{
+        backgroundImage: `url(${imgurl})`}}/>
+        <div className="content">
+          <div className="title">{title.toUpperCase()}</div>
+          <div className="subtitle">Shop Now</div>
+        </div>
+    </div>
+  );
+}
+
+export default Menuitem;
--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,17 +1,12 @@
 
 import './Homepage.scss';
-import Listitem from '../../components/list-item/listitem';
+import Directory from '../../components/directory/directory';
+
 function Homepage() {
   return (
     <div className="homepage">
       <h1>Homepage</h1>
-      <div className='directory-list'>
-        <Listitem title='Anime-1' subtitle='Manga' />
-        <Listitem title='Anime-2' subtitle='Manga' />
-        <Listitem title='Anime-3' subtitle='Manga' />
-        <Listitem title='Anime-4' subtitle='Manga' />
-        <Listitem title='Anime-5' subtitle='Manga' />
-      </div>
+      <Directory />
     </div>
   );
 }

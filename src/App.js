066e279bb1b311cbecda4207156037199b7ddab5
Sudhanshu <deepak@Deepaks-MacBook-Air.local>
2023-04-23 21:14:10 +0530

    created header component with styles and routed to shop page and home page

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import {Switch,Route} from 'react-router-dom';
 import Homepage from './pages/Homepage/Homepage';
 import ShopPage from './pages/ShopPage/ShopPage';
+import Header from './components/header/header';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route exact path="/shop" component={ShopPage} />

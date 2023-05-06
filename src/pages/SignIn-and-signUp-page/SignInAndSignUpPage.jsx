d52bfa0425c0b8099b32d7cfdbd12d2a5117bf69
Sudhanshu <deepak@Deepaks-MacBook-Air.local>
2023-05-06 15:57:01 +0530

    created sign up page and authenticated with firebase

--- src/pages/SignIn-and-signUp-page/SignInAndSignUpPage.jsx
+++ src/pages/SignIn-and-signUp-page/SignInAndSignUpPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import SignIn from "../../components/SignIn/SignIn";
+import SignUp from "../../components/signUp/signUp";
 import './SignInAndSignUpPage.scss'
 
 const SignInAndSignUpPage = () => (
     <div className="SignInAndSignUpPage">
         <SignIn />
+        <SignUp />
     </div>
 )
 export default SignInAndSignUpPage;
